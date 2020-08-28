34e55874cd44bc3f30343dea5d1dff5f428305bc
QuanDo <thienkimlove@gmail.com>
2020-08-28 15:07:13 +0700

    update

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -63,9 +63,10 @@
                             <div class="kt-header__topbar-wrapper" data-toggle="dropdown" data-offset="0px,0px">
                                 <div class="kt-header__topbar-user">
                                     <span class="kt-header__topbar-welcome kt-hidden-mobile">Xin chào,</span>
+                                    <span class="kt-header__topbar-username kt-hidden-mobile">{{ session()->get(\App\Helpers::SESSION_NAME)->name }}</span>
 
                                     <!--use below badge element instead the user avatar to display username's first letter(remove kt-hidden class to display it) -->
-                                    <span class="kt-badge kt-badge--username kt-badge--unified-success kt-badge--lg kt-badge--rounded kt-badge--bold">{{ session()->get(\App\Helpers::SESSION_NAME)->name }}</span>
+                                    <span class="kt-badge kt-badge--username kt-badge--unified-success kt-badge--lg kt-badge--rounded kt-badge--bold">{{ \Illuminate\Support\Str::limit(session()->get(\App\Helpers::SESSION_NAME)->name, 1, '') }}</span>
                                 </div>
                             </div>
                             <div class="dropdown-menu dropdown-menu-fit dropdown-menu-right dropdown-menu-anim dropdown-menu-top-unround dropdown-menu-xl">
