6f57ae249c626464eb9272207ad9d515c562024c
QuanDo <thienkimlove@gmail.com>
2020-09-10 11:07:41 +0700

    update

--- resources/views/vendor/backpack/crud/buttons/template_excel_user.blade.php
+++ resources/views/vendor/backpack/crud/buttons/template_excel_user.blade.php
@@ -1,3 +1,3 @@
-<a  href="{{ url($crud->route.'/'.$entry->getKey().'/downloadExcelUser') }}" class="btn btn-xs btn-elevate">
+<a  href="{{ url($crud->route.'/'.$entry->getKey().'/downloadExcelUser') }}" class="btn btn-xs btn-pinterest">
     <i class="fa fa-balance-scale"></i>Tải mẫu nhân viên
 </a>
