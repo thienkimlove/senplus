35741237a25c3f858a7464834e1bd9d7ff50dbc8
QuanDo <thienkimlove@gmail.com>
2020-10-09 17:44:32 +0700

    update

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -6,6 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
     <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5"/>
 
+    <meta name="robots" content="noindex">
+
+    <meta name="googlebot" content="noindex">
+
     <link rel="shortcut icon" href="/frontend/assets/img/favicon.png"/>
     <link rel="icon" href="/frontend/assets/img/favicon.png" type="image/x-icon"/>
     <link rel="shortcut icon" href="/frontend/assets/img/favicon.png" type="image/vnd.microsoft.icon"/>
