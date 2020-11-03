65b3f4b1a9be0c018f61de5bdb74eb4cab6eb787
QuanDo <thienkimlove@gmail.com>
2020-11-03 16:02:49 +0700

    update

--- resources/views/frontend/flash-message.blade.php
+++ resources/views/frontend/flash-message.blade.php
@@ -1,6 +1,9 @@
 @if ($message = session()->get('general_message'))
-    <div class="alert alert-info alert-block">
-        <button type="button" class="close" data-dismiss="alert">×</button>
-        <strong>{{ $message }}</strong>
+   <div class="alert alert-info alert-block">
+        <div class="alert-overlay"></div>
+        <div class="content">
+            <button type="button" class="close" data-dismiss="alert">×</button>
+            <strong>{{ $message }}</strong>
+        </div>
     </div>
 @endif
