7e9b9b9fb66e3eb473f5affd54b58e8d338e03e6
QuanDo <thienkimlove@gmail.com>
2020-12-16 09:46:52 +0700

    update

--- resources/views/frontend/blog.blade.php
+++ resources/views/frontend/blog.blade.php
@@ -54,6 +54,7 @@
                 @foreach ($popularPosts as $popularPost)
                     <div class="post">
                         <a href="{{ url($popularPost->slug.'.html') }}" class="imgThumb" title="" style="background: url({{ url($popularPost->image) }}) center center no-repeat #3aadbb;"></a>
+                        <div class="parentCategory">{{ \App\Helpers::getMainTopicPost($popularPost) }}</div>
                         <a href="{{ url($popularPost->slug.'.html') }}" class="title" title="{{ $popularPost->name }}">
                             {{ $popularPost->name }}
                         </a>
