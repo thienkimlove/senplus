2297d91bcd0efd498f41a0f56a3ab66c6841cffd
QuanDo <thienkimlove@gmail.com>
2021-05-11 14:11:44 +0700

    update

--- resources/views/frontend/privacy.blade.php
+++ resources/views/frontend/privacy.blade.php
@@ -7,7 +7,7 @@
 
 @section('content')
     <main>
-        <div class="contain pa">
+        <div class="topBlog">
             <h1>Privacy Policy</h1>
             <p>Last updated: May 11, 2021</p>
             <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
